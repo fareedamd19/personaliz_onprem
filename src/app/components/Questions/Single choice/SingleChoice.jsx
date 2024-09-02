4203602dbf971c9e1deeb8058de3e5f34e7fb585
Vishi Chaurasiya <vishichaurasiya@gmail.com>
2024-09-02 17:29:20 +0530

    formatted SingleChoice file

--- src/app/components/Questions/Single choice/SingleChoice.jsx
+++ src/app/components/Questions/Single choice/SingleChoice.jsx
@@ -1,49 +1,56 @@
-import { useGlobalStoreContext } from '@/app/context/GlobalStoreContext'
-import React, { Fragment, useEffect, useState } from 'react'
-import BranchOption from './Options/BranchOption';
-import UrlOption from './Options/UrlOption';
-import QuestionTitle from '../../Question Container/QuestionTitle';
-
-
+import { useGlobalStoreContext } from "@/app/context/GlobalStoreContext";
+import React, { Fragment, useEffect, useState } from "react";
+import BranchOption from "./Options/BranchOption";
+import UrlOption from "./Options/UrlOption";
+import QuestionTitle from "../../Question Container/QuestionTitle";
 
 const SingleChoice = () => {
+  const {
+    isQuestionOnTopOfVideo,
+    currentQuestionData,
+    fontThemeObj,
+    is_RTL,
+    getBackgroundColorForTitle,
+  } = useGlobalStoreContext();
 
-const {isQuestionOnTopOfVideo,currentQuestionData,fontThemeObj,is_RTL,getBackgroundColorForTitle}=useGlobalStoreContext()
-   
-    const[options,setOptions]=useState([])
-   
+  const [options, setOptions] = useState([]);
 
-    useEffect(()=>{
-        const optionsObj=JSON.parse(currentQuestionData.current.options)
-        for(let key in optionsObj){
-            setOptions(prev=>[...prev,optionsObj[key]])
-        }
-return ()=>{
-    setOptions([])
-}
-    },[currentQuestionData])
-
+  useEffect(() => {
+    const optionsObj = JSON.parse(currentQuestionData.current.options);
+    for (let key in optionsObj) {
+      setOptions((prev) => [...prev, optionsObj[key]]);
+    }
+    return () => {
+      setOptions([]);
+    };
+  }, [currentQuestionData]);
 
   return (
     <>
-    <section className={`w-[90%] mx-auto mt-4`}>
-    {currentQuestionData.current.text&&<QuestionTitle/>}
+      <section className={`w-[90%] mx-auto mt-4`}>
+        {currentQuestionData.current.text && <QuestionTitle />}
 
-    <div className={`w-full h-full flex ${!isQuestionOnTopOfVideo?'flex-col gap-7':'gap-2 md:gap-4'} flex-wrap mt-3`}>
-    {options.length>0&&options.map((option,index)=>{
-    return <Fragment key={index}>
-    {option.type==='branch'?
-    <BranchOption option={option} index={index} />
-    :
-    <UrlOption option={option} index={index} />
-    }
-    </Fragment>
-    })}
+        <div
+          className={`w-full h-full flex ${
+            !isQuestionOnTopOfVideo ? "flex-col gap-7" : "gap-2 md:gap-4"
+          } flex-wrap mt-3`}
+        >
+          {options.length > 0 &&
+            options.map((option, index) => {
+              return (
+                <Fragment key={index}>
+                  {option.type === "branch" ? (
+                    <BranchOption option={option} index={index} />
+                  ) : (
+                    <UrlOption option={option} index={index} />
+                  )}
+                </Fragment>
+              );
+            })}
         </div>
-
-        </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default SingleChoice
+export default SingleChoice;
