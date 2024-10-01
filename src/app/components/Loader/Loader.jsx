383b36113ba224eff80344f7386158d655e36469
Vishi Chaurasiya <vishichaurasiya@gmail.com>
2024-10-01 18:03:06 +0530

    added loaderfor middle-east server

--- src/app/components/Loader/Loader.jsx
+++ src/app/components/Loader/Loader.jsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useRef } from "react";
 import styles from "./Loader.module.css";
 import Image from "next/image";
+import MoonLoader from "react-spinners/MoonLoader";
 
 const Loader = ({ server_personaliz_branding }) => {
   const loaderInnerRef = useRef(null);
@@ -34,29 +35,34 @@ const Loader = ({ server_personaliz_branding }) => {
 
   return (
     <section className="w-full h-screen flex flex-col items-center justify-center bg-white absolute top-0 left-0 z-[10000]">
-      <div className="w-[117px] h-[117px]">
-        {server_personaliz_branding === "none" ? (
-          <Image
-            src={
-              "https://personaliz.s3.ap-south-1.amazonaws.com/Personaliz+Logos/Personaliz_custom+loader.gif"
-            }
-            height={117}
-            width={117}
-            alt="personaliz logo"
-          />
-        ) : (
-          <video
-            className={styles.videoElm}
-            src={
-              "https://personaliz.s3.ap-south-1.amazonaws.com/PersonalizBlackLogoAnimatedForLoading.mp4"
-            }
-            playsInline
-            muted
-            autoPlay
-            loop
-          ></video>
-        )}
-      </div>
+      {process.env.NEXT_PUBLIC_SERVER_ORIGIN === "middle-east" ? (
+        <MoonLoader />
+      ) : (
+        <div className="w-[117px] h-[117px]">
+          {server_personaliz_branding === "none" ? (
+            <Image
+              src={
+                "https://personaliz.s3.ap-south-1.amazonaws.com/Personaliz+Logos/Personaliz_custom+loader.gif"
+              }
+              height={117}
+              width={117}
+              alt="personaliz logo"
+            />
+          ) : (
+            <video
+              className={styles.videoElm}
+              src={
+                "https://personaliz.s3.ap-south-1.amazonaws.com/PersonalizBlackLogoAnimatedForLoading.mp4"
+              }
+              playsInline
+              muted
+              autoPlay
+              loop
+            ></video>
+          )}
+        </div>
+      )}
+
       <div className={styles.interactly_loader_outer_cont}>
         <div
           ref={loaderInnerRef}
