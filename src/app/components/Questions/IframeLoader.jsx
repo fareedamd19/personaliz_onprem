39b37893268b6d5ae5a70209d2134b8d71f039c6
Vishi Chaurasiya <vishichaurasiya@gmail.com>
2024-10-01 17:22:40 +0530

    formatted IframeLoader.jsx and Loader.jsx file

--- src/app/components/Questions/IframeLoader.jsx
+++ src/app/components/Questions/IframeLoader.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const IframeLoader = () => {
   return (
     <>
-<div className='shadow-lg w-full h-[65vh] rounded-md flex items-center justify-center bg-white'>
-<div className='w-[117px] h-[117px]'>
-<video className={`w-full h-full object-cover object-center`} src={"https://personaliz.s3.ap-south-1.amazonaws.com/PersonalizBlackLogoAnimatedForLoading.mp4"} playsInline muted autoPlay loop></video>
-</div>
-</div>
+      <div className="shadow-lg w-full h-[65vh] rounded-md flex items-center justify-center bg-white">
+        <div className="w-[117px] h-[117px]">
+          <video
+            className={`w-full h-full object-cover object-center`}
+            src={
+              "https://personaliz.s3.ap-south-1.amazonaws.com/PersonalizBlackLogoAnimatedForLoading.mp4"
+            }
+            playsInline
+            muted
+            autoPlay
+            loop
+          ></video>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default IframeLoader
+export default IframeLoader;
