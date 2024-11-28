77de141afd3201ec8f589e9d5d7b07a98695584a
Utkarsh <developer.interactly@gmail.com>
2024-11-28 13:38:44 +0530

    formatted the file

--- src/app/components/Main Entry Point/MainEntryPoint.js
+++ src/app/components/Main Entry Point/MainEntryPoint.js
@@ -4,16 +4,16 @@ import React from 'react'
 import { GlobalStoreProvider } from '@/app/context/GlobalStoreContext'
 import Dashboard from '../Dashboard/Dashboard'
 
-const MainEntryPoint = ({server_personaliz_branding}) => {
-   
-   
+const MainEntryPoint = ({ server_personaliz_branding }) => {
+
+
 
   return (
-   <>
-  <GlobalStoreProvider> 
-<Dashboard server_personaliz_branding={server_personaliz_branding}/>
-</GlobalStoreProvider>
-   </>
+    <>
+      <GlobalStoreProvider>
+        <Dashboard server_personaliz_branding={server_personaliz_branding} />
+      </GlobalStoreProvider>
+    </>
   )
 }
 
